609eb09f917f38a0364a0299ca06bb5c956c6d6e
BugHunter-X <askzenai@gmail.com>
2025-01-27 13:20:09 +0100

    Add bugSolution.ts file

---
+++ bugSolution.ts
@@ -0,0 +1,43 @@
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+function subtract(a: number, b: number): number {
+  return a - b;
+}
+
+function multiply(a: number, b: number): number {
+  return a * b;
+}
+
+function divide(a: number, b: number): number {
+  if (b === 0) {
+    throw new Error('Cannot divide by zero');
+  }
+  return a / b;
+}
+
+let result: number;
+
+try {
+  result = divide(9, 0);
+  console.log(result);
+} catch (error) {
+  console.error('An error occurred:', error.message);
+}
+
+result = add(5, 3);
+console.log(result); // Output: 8
+
+result = subtract(10, 4);
+console.log(result); // Output: 6
+
+result = multiply(7, 2);
+console.log(result); // Output: 14
+
+try {
+  result = divide(9, 3);
+  console.log(result); //Output:3
+} catch (error) {
+  console.error('An error occurred:', error.message);
+}
